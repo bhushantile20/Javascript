7f1a2f8f7c218d12ac2858ddc642a55f137da018
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-07-12 23:48:05 +0530

    Add files via upload

---
+++ cube.java
@@ -0,0 +1,9 @@
+public class  cube
+{
+    public static void main(String args[])
+    {
+        int side=5;
+        float volume=side*side* side;
+        System.out.println("Volume of cube:"+volume);
+    }
+}
