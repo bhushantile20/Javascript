9044d68ca91401f8f3ca1a2aafc24faf6ff230b4
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2024-07-12 23:50:55 +0530

    reverse the no

---
+++ reverseNum.java
@@ -0,0 +1,20 @@
+public class reverseNum
+{
+    public static void main(String argsp[])
+    {
+        int rev=0;
+        int num=1234;
+        int no=num;
+        while(num>0)
+        {
+            int rem=num %10;
+            rev=rem+(rev*10);
+            num=num/10;
+
+        }
+
+        System.out.println("Number="+no);
+        System.out.println("Reverse"+rev);
+        
+    }
+}
